761121ce41477dbaf1a3b0a3ba50cecfd2e5a385
murali <murali@sieplinc.com>
2022-11-29 14:25:32 +0530

    lock file

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -16,11 +16,7 @@ terraform {
     }    
   }
 # Terraform State Storage to Azure Storage Container (Values will be taken from Azure DevOps)
-  backend "azurerm" {
-   resource_group_name   = "terraform-storage-rg"
-    storage_account_name  = "terraformstate221"
-    container_name        = "tfstatefiles"
-    key                   = "dev-terraform.tfstate"    
+  backend "azurerm" {  
   }   
 }
 
